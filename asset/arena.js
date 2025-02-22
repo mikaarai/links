83a6cc6ec51a8fefb96260c2815a28d26a2b6bcb
Mika Arai <araim330@newschool.edu>
2025-02-22 15:06:01 -0500

    html: removed temp link and added each media ID to connect to arena API under li; CSS:  reorder for readability and added hide/show function; JS: added a filter function on the bottom to show/hide items

--- asset/arena.js
+++ asset/arena.js
@@ -1,42 +1,46 @@
-// This allows us to process/render the descriptions, which are in Markdown!
+// someone who already made it online and you can just copy it
 // More about Markdown: https://en.wikipedia.org/wiki/Markdown
-let markdownIt = document.createElement('script')
-markdownIt.src = 'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
-document.head.appendChild(markdownIt)
+	let markdownIt = document.createElement('script')
+	markdownIt.src = 'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
+	document.head.appendChild(markdownIt)
 
 
+// connecting arena page
+	let channelSlug = 'philosophers-cave' 
 
-let channelSlug = 'philosophers-cave' 
+	// First, let’s lay out some *functions*, starting with our basic metadata:
+	// Let = function that allows you to command something or create a new fresh variable
+	// Connecting with your html eg. ‘#channel-title’
+	let placeChannelInfo = (data) => {
+		// Target some elements in your HTML:
+		let channelTitle = document.querySelector('#channel-title')
+		let channelDescription = document.querySelector('#channel-description')
+		let channelCount = document.querySelector('#channel-count')
+		let channelLink = document.querySelector('#channel-link')
 
-// First, let’s lay out some *functions*, starting with our basic metadata:
-let placeChannelInfo = (data) => {
-	// Target some elements in your HTML:
-	let channelTitle = document.querySelector('#channel-title')
-	let channelDescription = document.querySelector('#channel-description')
-	let channelCount = document.querySelector('#channel-count')
-	let channelLink = document.querySelector('#channel-link')
+		// Then set their content/attributes to our data:
+		// innerHTML = assigning task or changing the text like i want this to be this (i want channelTitle to be data.title)
+		channelTitle.innerHTML = data.title
+		// channelDescription.innerHTML = window.markdownit().render(data.metadata.description) // Converts Markdown → HTML
+		channelCount.innerHTML = data.length
+		channelLink.href = `https://www.are.na/channel/${channelSlug}`
+	}
 
-	// Then set their content/attributes to our data:
-	channelTitle.innerHTML = data.title
-	channelDescription.innerHTML = window.markdownit().render(data.metadata.description) // Converts Markdown → HTML
-	channelCount.innerHTML = data.length
-	channelLink.href = `https://www.are.na/channel/${channelSlug}`
-}
-
-// Then our big function for specific-block-type rendering:
-let renderBlock = (block) => {
-	// To start, a shared `ul` where we’ll insert all our blocks
-	let channelBlocks = document.querySelector('#channel-blocks')
-	let imageBlocks = document.querySelector('#image-blocks')
-	let textBlocks = document.querySelector('#text-blocks')
-	let videoBlocks = document.querySelector('#video-blocks')
+	// Then our big function for specific-block-type rendering:
+	let renderBlock = (block) => {
+		// To start, a shared `ul` where we’ll insert all our blocks
+		let channelBlocks = document.querySelector('#channel-blocks')
+		// let imageBlocks = document.querySelector('#image-blocks')
+		// let textBlocks = document.querySelector('#text-blocks')
+		// let videoBlocks = document.querySelector('#video-blocks')
 
 
-	// Links!
-	if (block.class_name == 'Link') {
-		let linkItem =
-			`
-			<li>
+		// Links!
+		if (block.class_name == 'Link') {
+			let linkItem =
+				`
+				// added a class so that i can target it on css
+				<li class="link-block">
 				<p><em>Link</em></p>
 				<picture>
 					<source media="(max-width: 428px)" srcset="${ block.image.thumb.url }">
@@ -48,40 +52,40 @@ let renderBlock = (block) => {
 				<p><a href="${ block.source.url }">See the original ↗</a></p>
 			</li>
 			`
-		channelBlocks.insertAdjacentHTML('beforeend', linkItem)
-	}
+				// linkItem will be place here
+				// beforeend = before the end of the element
+				// insertAdjacentHTML = its right next to (telling the computer to put right next to the html)
+				channelBlocks.insertAdjacentHTML('beforeend', linkItem)
+			}
+		
 
-	// Images!
-	else if (block.class_name == 'Image') { // Use 'class_name' instead of 'class'
-		let photoItem = `
-			<li>
-				<picture>
-					${ block.image.thumb ? `<source media="(max-width: 428px)" srcset="${ block.image.thumb.url }">` : '' }
-					${ block.image.large ? `<source media="(max-width: 640px)" srcset="${ block.image.large.url }">` : '' }
-					<img src="${ block.image.original.url }" alt="Image from Are.na">
-				</picture>
-			</li>
-		`;
-		imageBlocks.insertAdjacentHTML('beforeend', photoItem);
-	}
-
+		// Images!
+		else if (block.class == 'Image') { 
+			let photoItem = `
+				<li class="image-block">
+					<picture>
+						${ block.image.thumb ? `<source media="(max-width: 428px)" srcset="${ block.image.thumb.url }">` : '' }
+						${ block.image.large ? `<source media="(max-width: 640px)" srcset="${ block.image.large.url }">` : '' }
+						<img src="${ block.image.original.url }" alt="Image from Are.na">
+					</picture>
+				</li>
+			`
+			channelBlocks.insertAdjacentHTML('beforeend', photoItem);
+		}
+		
 		// Text!
-		else if (block.class_name == 'Text') {
+		else if (block.class == 'Text') {
 			let textItem = `
 				<li class="text-block">
 					<p><em>Text</em></p>
 					<p>${block.content_html}</p>
 				</li>
-			`;
-		
-			let textBlocks = document.querySelector('#text-blocks');
-    		if (textBlocks) {
-      			textBlocks.insertAdjacentHTML('beforeend', textItem);
-    		}
+			`
+      		textItem.insertAdjacentHTML('beforeend', textItem);
 		}
 	
 		// Uploaded (not linked) media…
-		else if (block.class_name == 'Attachment') {
+		else if (block.class == 'Attachment') {
 			let attachment = block.attachment.content_type // Save us some repetition
 	
 			// Uploaded videos!
@@ -109,7 +113,7 @@ let renderBlock = (block) => {
 				// …still up to you, but here’s an `audio` element:
 				let audioItem =
 					`
-					<li>
+					<li class="audio-block">
 						<p><em>Audio</em></p>
 						<audio controls src="${ block.attachment.url }"></audio>
 					</li>
@@ -120,7 +124,7 @@ let renderBlock = (block) => {
 		}
 	
 		// Linked media…
-		else if (block.class_name == 'Media') {
+		else if (block.class == 'Media') {
 			let embed = block.embed.type
 	
 			// Linked video!
@@ -128,7 +132,7 @@ let renderBlock = (block) => {
 				// …still up to you, but here’s an example `iframe` element:
 				let linkedVideoItem =
 					`
-					<li>
+					<li class="video-block">
 						<p><em>Linked Video</em></p>
 						${ block.embed.html }
 					</li>
@@ -155,12 +159,10 @@ let renderBlock = (block) => {
 				<p><a href="https://are.na/${ user.slug }">Are.na profile ↗</a></p>
 			</address>
 			`
-		container.insertAdjacentHTML('beforeend', userAddress)
+		// container.insertAdjacentHTML('beforeend', userAddress)
 	}
-	
-	
-	
-	// Now that we have said what we can do, go get the data:
+		
+	// API stuff - Now that we have said what we can do, go get the data:
 	fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-store' })
 		.then((response) => response.json()) // Return it as JSON data
 		.then((data) => { // Do stuff with the data
@@ -178,3 +180,61 @@ let renderBlock = (block) => {
 			data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers))
 			renderUser(data.user, channelUsers)
 		})
+
+
+// function to filter basically show or hide item based on media type
+// if "Show All" is selected, it removes the hide class from all items
+// guidance from class/Eric loom video and went through with tutor 
+		let channelBlocks = document.querySelector('#channel-blocks');
+		let showVideoButton = document.querySelector('#show-video-button');
+		let showImageButton = document.querySelector('#show-image-button');
+		let showTextButton = document.querySelector('#show-text-button');
+		let showLinkButton = document.querySelector('#show-link-button');
+		let showAudioButton = document.querySelector('#show-audio-button');
+		let showAllButton = document.querySelector('#show-all-button');
+		
+		function showCategory(categoryClass) {
+			// remove all possible filter classes
+			channelBlocks.classList.remove('show-video', 'show-image', 'show-text', 'show-link', 'show-audio', 'show-all');
+		
+			// add the correct category class
+			channelBlocks.classList.add(categoryClass);
+		
+			// get all the list items in the channel blocks
+			const allItems = channelBlocks.querySelectorAll('li');
+		
+			// Show or hide items based on the selected category
+			allItems.forEach(item => {
+				if (categoryClass === 'show-all') {
+					item.classList.remove('hide'); 
+				} else {
+					if (item.classList.contains(categoryClass.replace('show-', '') + '-block')) {
+						item.classList.remove('hide'); 
+					} else {
+						item.classList.add('hide'); 
+					}
+				}
+			});
+		
+			// to check if the correct class is applied. it is!
+			console.log("Applied class:", channelBlocks.classList);
+		}
+		
+		
+		// assigning event listeners
+		showVideoButton.onclick = () => showCategory('video');
+		showImageButton.onclick = () => showCategory('image');
+		showTextButton.onclick = () => showCategory('text');
+		showLinkButton.onclick = () => showCategory('link');
+		showAudioButton.onclick = () => showCategory('audio');
+		
+		// showing all button displays everything
+		showAllButton.onclick = () => {
+			channelBlocks.querySelectorAll('li').forEach(li => {
+				li.classList.remove('hide'); // remove or hide class from all items
+			});
+			console.log("Showing all items");
+		};
+
+
+
