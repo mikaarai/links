241edcb3ca0023dbe4154240eee42e36d63ea479
Mika Arai <araim330@newschool.edu>
2025-05-11 18:56:32 -0400

    add back to top

--- asset/arena.js
+++ asset/arena.js
@@ -260,3 +260,17 @@ let closeButton = dialog.querySelector('.close-button');
 };
 
 
+// back to top
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+});
+
+backToTopButton.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
